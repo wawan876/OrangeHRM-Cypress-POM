d19db1a70799f49267ee5b86aa3701d5fa6750e6
wawan <wawampras@gmail.com>
2025-08-28 17:41:31 +0700

    penamabahan lupa password

---
+++ cypress/pageobject/forgot-password.js
@@ -0,0 +1,16 @@
+const forgotpassword ={
+    
+    forgotpassword(){
+    cy.get(".orangehrm-login-forgot-header").click();
+    },
+    usernameresetpass(){
+        cy.get("[name='username']").type("Admin")
+    },
+    btnresetpass(){
+        cy.get(".oxd-button--secondary").click();
+    },
+    assertSuccessResetpass(){
+        cy.get(".oxd-text--h6").should("contain.text, Reset Password link sent successfully");
+    }
+}
+export default forgotpassword;
